e31871defcaf8edd23b13d677966d895c729c58c
Tyler Fonzi <tyler.a.fonzi@gmail.com>
2025-01-01 18:03:20 -0500

    added extra permissions for github iam role

--- terraform/github-actions-role.tf
+++ terraform/github-actions-role.tf
@@ -121,6 +121,8 @@ data "aws_iam_policy_document" "github_actions_policy" {
       "s3:DeleteBucket",
       "s3:PutBucketPolicy",
       "s3:GetBucketPolicy",
+      "s3:GetBucketAcl",
+      "s3:PutBucketAcl",
       "s3:PutObject",
       "s3:GetObject",
       "s3:DeleteObject",
